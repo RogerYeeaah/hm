01c6920ab902946cbbf71aaff908763f07f7ea7b
RogerYeeaah <77545161+RogerYeeaah@users.noreply.github.com>
2022-01-12 23:17:37 +0800

    2022/1/12

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ gulp.task('sass', function () {
         // .on('error', errorLog)
         // .pipe(gulpPlumber())
         .pipe(sass({
-            outputStyle: 'nested '
+            outputStyle: 'expanded'
         }))
         .pipe(autoprefixer({
             overrideBrowserslist: ["last 5 versions", "Android >= 4.0"],
